8fde80f0716b339cb93c9fb31d6a961b7ffa92f9
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-07-22 01:06:48 +0100

    Ball shader spawn

--- src/tendrils/spawn/ball/index.js
+++ src/tendrils/spawn/ball/index.js
@@ -4,8 +4,13 @@ import vert from '../../shaders/screen/index.vert';
 import frag from './index.frag';
 
 export default (gl) => ({
+    uniforms: {
+        radius: 1,
+        speed: 0.01
+    },
+
     spawn: shader(gl, vert, frag),
     respawn(tendrils) {
-        tendrils.respawnShader(this.spawn);
+        tendrils.respawnShader(this.spawn, this.uniforms);
     }
 });
