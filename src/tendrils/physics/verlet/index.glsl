2af0b168e2bcc5640688fa715c1bd707ae22806e
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-08-16 17:38:34 +0100

    Verlet JS

--- src/tendrils/physics/verlet/index.glsl
+++ src/tendrils/physics/verlet/index.glsl
@@ -2,14 +2,14 @@
  * Verlet integration in 2D. See slide 26 of the second part of
  * [Acko's Animating Your Way to Glory](http://acko.net/blog/animate-your-way-to-glory/).
  *
- * @param acc Accelration.
- * @param pos Position.
- * @param lastPos Last position.
+ * @param acc Acceleration.
+ * @param pos1 Position.
+ * @param pos0 Last position.
  * @param dt Time elapsed since the last frame.
- * @return The new position.
+ * @return The new position (`pos2`).
  */
-vec2 verlet(vec2 acc, vec2 pos, vec2 lastPos, float dt) {
-    return (2.0*pos)-lastPos+(acc*dt*dt);
+vec2 verlet(vec2 acc, vec2 pos1, vec2 pos0, float dt) {
+    return (2.0*pos1)-pos0+(acc*dt*dt);
 }
 
 #pragma glslify: export(verlet)
--- src/tendrils/physics/verlet/index.js
+++ src/tendrils/physics/verlet/index.js
@@ -2,12 +2,12 @@
  * Verlet integration in 2D. See slide 26 of the second part of
  * [Acko's Animating Your Way to Glory](http://acko.net/blog/animate-your-way-to-glory/).
  *
- * @param acc Accelration.
- * @param pos Position.
- * @param lastPos Last position.
+ * @param acc Acceleration.
+ * @param pos1 Position.
+ * @param pos0 Last position.
  * @param dt Time elapsed since the last frame.
- * @return The new position.
+ * @return The new position (`pos2`).
  */
-export const verlet = (acc, pos, lastPos, dt) => (2.0*pos)-lastPos+(acc*dt*dt);
+export const verlet = (acc, pos1, pos0, dt) => (2.0*pos1)-pos0+(acc*dt*dt);
 
 export default verlet;
