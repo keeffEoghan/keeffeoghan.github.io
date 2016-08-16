6864577881f54ce7cc84e89cbaea9bd2449accc7
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-08-16 18:35:45 +0100

    Todos

---
+++ src/tendrils/audio/index.js
@@ -0,0 +1,9 @@
+/**
+ * Interpret useful info (velocity and higher order) from an audio FFT analysis,
+ * and provide hooks for binding to WebGL (data textures etc).
+ *
+ * @see  https://github.com/stackgl/gl-audio-analyser/blob/master/index.js
+ */
+
+import analyser from 'web-audio-analyser';
+
