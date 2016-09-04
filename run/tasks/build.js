cfe600758a01d80707b0d3d059c15fbe4722b23e
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-09-05 00:43:58 +0100

    Removing unused refs.

--- run/tasks/build.js
+++ run/tasks/build.js
@@ -8,9 +8,6 @@
  *  gulp build
  */
 
-var gulp = require('gulp'),
-    chalk = require('chalk'),
-    args = require('yargs').argv,
-    globalSettings = require('../config');
+var gulp = require('gulp');
 
 gulp.task('build', ['html', 'assets', 'custom-deps', 'styles', 'scripts']);
