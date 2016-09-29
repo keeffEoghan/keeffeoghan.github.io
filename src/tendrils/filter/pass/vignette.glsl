920e329e36ec84414463d5b48de3b71867111733
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-09-29 23:56:47 +0100

    Tweaking the vignette in response to feedback

--- src/tendrils/filter/pass/vignette.glsl
+++ src/tendrils/filter/pass/vignette.glsl
@@ -1,18 +1,13 @@
-// @todo Tweak shape of vignette falloff.
+// @todo Uniform inputs for `const` values here.
 
-#pragma glslify: length2 = require(../../utils/length-2)
+#pragma glslify: bezier = require(../../utils/bezier)
 
 const vec2 midUV = vec2(0.5);
-const float limit = 0.5*0.5;
+const vec3 curve = vec3(0.0, 0.6, 1.0);
+const float limit = 0.5;
 
 vec4 pass(vec2 uv, vec4 pixel) {
-    return vec4(pixel*max(0.0, limit-length2(uv-midUV)));
+    return vec4(pixel*max(0.0, bezier(curve, limit-length(uv-midUV))));
 }
-
-// const float limit = 0.5;
-
-// vec4 pass(vec2 uv, vec4 pixel) {
-//     return vec4(pixel*max(0.0, limit-length(uv-midUV)));
-// }
 
 #pragma glslify: export(pass)
