88b31c1b1ba4ca15be9391b8ef34170908963697
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-11-17 04:14:13 +0000

    Hack to be able to animate/update the audio order we use at run-time

--- src/torch/animations/vaccines.js
+++ src/torch/animations/vaccines.js
@@ -1,23 +1,14 @@
 import * as colors from '../colors';
 
 export default (player) => {
-    player.tracks.a
-        .smoothTo({
-            to: {
-                ringRadius: 0.1,
-                lightColor: colors.orange,
-                fadeColor: colors.lightBlueA
-            },
-            time: 3000,
-            ease: [0, 0.95, 1]
-        })
-        .smoothTo({
-            to: {
-                ringRadius: 1,
-                lightColor: colors.pink,
-                fadeColor: colors.darkBlue
-            },
-            time: 13000,
-            ease: [0, 0.95, 1]
-        });
+    // player.tracks.a
+    //     .smoothTo({
+    //         to: {
+    //             ringRadius: 1.4,
+    //             lightColor: colors.orange,
+    //             fadeColor: colors.lightBlueA
+    //         },
+    //         time: 10000,
+    //         ease: [0, 0.95, 1]
+    //     });
 };
