6a6c3b9fd972ae137de27506b0af285081e4241b
Eoghan O'Keeffe <eoghan@rehabstudio.com>
2016-11-18 05:11:08 +0000

    Loads of cool accidental complex surfaces froming for Tall Children

--- src/torch/animations/index.js
+++ src/torch/animations/index.js
@@ -3,11 +3,13 @@ import yearsAndYears from './years-and-years';
 import blaenavon from './blaenavon';
 import frankTurner from './frank-turner';
 import tomTheLion from './tom-the-lion';
+import tallChildren from './tall-children';
 
 export default {
     vaccines,
     yearsAndYears,
     blaenavon,
     frankTurner,
-    tomTheLion
+    tomTheLion,
+    tallChildren
 };
